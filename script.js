283e0f6a26977f71cdf78da13f4c55c9cf4805cd
Nitin Shukla <55484433+thenitinshukla@users.noreply.github.com>
2025-02-27 23:41:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function loadResearchMetrics() {
             // Fallback data since no real API is available
             const fallbackData = {
                     citations: 796,
-                    publications: 40,
+                    publications: 34,
                     hIndex: 15,
                     citationYears: [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
                     citationsPerYear: [7, 19, 16, 20, 23, 28, 22, 13, 40, 49, 51, 60, 61, 65, 51, 84, 77, 100, 7]
