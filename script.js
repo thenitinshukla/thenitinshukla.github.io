da1572bc105b40722b6eb50366be1adf7997ae0e
Nitin Shukla <n.shukla@cineca.it>
2025-03-03 16:19:36 +0100

    correction

--- script.js
+++ script.js
@@ -535,7 +535,7 @@ document.addEventListener('DOMContentLoaded', function() {
             
             `,
             date: "August 10, 2022",
-            category: "hpc",
+            category: "Technology",
             featured: true,
             imageUrl: "images/gpu-architecture.jpg",
             readTime: "3 min read",
